5c07534cee0263d66f1cd836ee505d6594f208f4
Martin Burchell <martinb@aptivate.org>
2016-04-21 11:00:52 +0100

    Fixed abort method to have correct signature and return value

--- ckanext/googleauth/plugin.py
+++ ckanext/googleauth/plugin.py
@@ -28,7 +28,7 @@ import uuid
 import pylons
 import pylons.config as config
 import ckan.lib.helpers as helpers
-import requests 
+import requests
 import re
 
 
@@ -71,7 +71,7 @@ class GoogleauthPlugin(plugins.SingletonPlugin):
     #verify email address within token
     def verify_email(self, token):
         res = requests.post('https://www.googleapis.com/oauth2/v3/tokeninfo?id_token='+token, verify=True)
-	
+
         if res.ok:
                 is_email_verified=json.loads(res.content)
                 if is_email_verified['email_verified'] == 'true':
@@ -88,7 +88,7 @@ class GoogleauthPlugin(plugins.SingletonPlugin):
     def get_ckanuser(self, user):
     	import ckan.model
 
-	user_ckan = ckan.model.User.by_name(user)	
+	user_ckan = ckan.model.User.by_name(user)
 
 	if user_ckan:
 		user_dict = toolkit.get_action('user_show')(data_dict={'id': user_ckan.id})
@@ -101,7 +101,7 @@ class GoogleauthPlugin(plugins.SingletonPlugin):
     #generates a strong password
     def get_ckanpasswd(self):
 	import datetime
-	import random	
+	import random
 
 	passwd = str(random.random())+ datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")+str(uuid.uuid4().hex)
 	passwd = re.sub(r"\s+", "", passwd, flags=re.UNICODE)
@@ -113,14 +113,14 @@ class GoogleauthPlugin(plugins.SingletonPlugin):
         #import pylons
 
 	#to revoke the Google token uncomment the code below
-    
+
 	#if 'ckanext-google-accesstoken' in pylons.session:
 	#    atoken = pylons.session.get('ckanext-google-accesstoken')
 	#    res = requests.get('https://accounts.google.com/o/oauth2/revoke?token='+atoken)
 	#    if res.status_code == 200:
-	#    	del pylons.session['ckanext-google-accesstoken']	    	
+	#    	del pylons.session['ckanext-google-accesstoken']
 	#    else:
-	#	raise GoogleAuthException('Token not revoked')	
+	#	raise GoogleAuthException('Token not revoked')
         if 'ckanext-google-user' in pylons.session:
             del pylons.session['ckanext-google-user']
         if 'ckanext-google-email' in pylons.session:
@@ -131,29 +131,29 @@ class GoogleauthPlugin(plugins.SingletonPlugin):
 
     #at every access the email address is checked. if it is authorized ckan username is created and access is given
     def login(self):
-	
+
     	params = toolkit.request.params
-	
+
 	if 'id_token' in params:
 		try:
-			mail_verified = self.verify_email(params['id_token'])		
+			mail_verified = self.verify_email(params['id_token'])
 		except GoogleAuthException, e:
 			toolkit.abort(500)
 
-		user_account = re.sub('[^A-Za-z0-9]+','_',mail_verified)	
+		user_account = re.sub('[^A-Za-z0-9]+','_',mail_verified)
 
-		user_ckan = self.get_ckanuser(user_account)		
-		 
+		user_ckan = self.get_ckanuser(user_account)
+
 		if not user_ckan:
 			user_ckan = toolkit.get_action('user_create')(
                     				context={'ignore_auth': True},
                     				data_dict={'email': mail_verified,
                                			'name': user_account,
-                               			'password': self.get_ckanpasswd()})	
-		
+                               			'password': self.get_ckanpasswd()})
+
 		pylons.session['ckanext-google-user'] = user_ckan['name']
         	pylons.session['ckanext-google-email'] = mail_verified
-            
+
 		#to revoke the Google token uncomment the code below
 		#pylons.session['ckanext-google-accesstoken'] = params['token']
             	pylons.session.save()
@@ -171,16 +171,7 @@ class GoogleauthPlugin(plugins.SingletonPlugin):
     def logout(self):
 	self._logout_user()
 
+    def abort(self, status_code=None, detail='', headers=None, comment=None):
+        self._logout_user()
 
-
-    def abort(self):
-	self._logout_user()
-
-
-
-
-
-
-
-
-
+        return (status_code, detail, headers, comment)
