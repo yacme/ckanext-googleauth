187c7e3d4d55cd844fd66bb26bb8df52ae384be0
Martin Burchell <martinb@aptivate.org>
2023-03-29 16:02:17 +0100

    Remove obsolete public folder

--- ckanext/googleauth/plugin.py
+++ ckanext/googleauth/plugin.py
@@ -56,7 +56,6 @@ class GoogleauthPlugin(plugins.SingletonPlugin, DefaultTranslation):
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
 
 
     #declare new helper functions
